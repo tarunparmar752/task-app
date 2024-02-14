fde3a9c99ed6431f88cb6f26f09b4291a265056c
Tarun Parmar <tarunparmar752@gmail.com>
2024-02-15 00:05:57 +0530

    logging user out

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
+
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -27,13 +29,31 @@ router.post("/users/login", async (req, res) => {
   }
 });
 
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
+router.post("/users/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.post("/users/logoutAll", auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get('/users/me', auth , async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
